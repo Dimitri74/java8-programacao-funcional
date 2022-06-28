b63d5cab8df2e7a2b61193611f1b4e80d63982b7
Marcus Dimitri <marcusdimitri7@gmail.com>
2022-06-28 20:58:53 -0300

    inicio do curso

---
+++ java-prog-funcional/src/javaProgamacaofuncional/Exec1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package javaProgamacaofuncional;
+
+import org.junit.Test;
+
+/**
+ * @author Marcus Dimitri
+ *
+ */
+public class Exec1 {
+	
+	
+	@Test
+	public void exec1() {
+		
+		System.out.println("Teste Java Funcional");
+		
+		
+		
+	}
+
+}
